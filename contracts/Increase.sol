cb60547109d1d03ba8680c7ef3f998d51465e85d
enzifiri <76253089+enzifiri@users.noreply.github.com>
2023-12-04 22:12:59 +0300

    Update Increase.sol

--- contracts/Increase.sol
+++ contracts/Increase.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Counter {
+    uint256 public count;  // Sayıyı tutan değişken
+
+    // Akıllı sözleşme oluşturulduğunda başlangıç değeri
+    constructor() {
+        count = 0;
+    }
+
+    // Sayıyı artıran fonksiyon
+    function increment() public {
+        count += 1;
+    }
+}
